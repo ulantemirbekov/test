0267ef147fade3679b875001596e2cdbcd15f2c7
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-10 19:40:21 +0200

    Update task_01.js

--- js/module_02/task_01.js
+++ js/module_02/task_01.js
@@ -1,1 +1,10 @@
 console.log('Module_02 Task_01');
+
+console.log('-----');
+
+const pets = ['Mango', 'Poly', 'Ajax'];
+console.log(pets)
+
+console.log(pets[0]);
+console.log(pets[1]);
+console.log(pets[2]);
