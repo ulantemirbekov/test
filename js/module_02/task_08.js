62c47ddd747e908eccdc61f687242bdcfc6b2f3b
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-13 00:41:16 +0200

    Create task_08.js

---
+++ js/module_02/task_08.js
@@ -0,0 +1,36 @@
+console.log('Module_02 Task_08');
+
+console.log('-----');
+
+'use strict';
+
+function reduceArray(array) {
+  
+    let total = 0;
+    
+    // if (!array.length) {
+    //     return 0;
+    // }
+  
+    for (let i = 0; i < array.length; i += 1) {
+        // console.log(array[i]);
+        total += array[i];
+    }
+   
+  return total;
+};
+
+console.log(reduceArray([1, 2, 3]));
+// 6
+
+console.log(reduceArray([-2, 0, 2]));
+// 0
+
+console.log(reduceArray([1, 2, 2.5]));
+// 5.5
+
+
+// Напиши функцию reduceArray(array), которая принимает числовой массив array.
+// Если массив array не пустой, необходимо посчитать сумму всех элементов массива, используя цикл for.
+// Для подсчета суммы используй переменную total.
+// Функция должна возвращать 0 если массив пустой и значение total в противном случае.
