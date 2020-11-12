e4b452145bd1a1a933b69fef8675073f9c9e3940
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-12 23:53:01 +0200

    Create task_07.js

---
+++ js/module_02/task_07.js
@@ -0,0 +1,40 @@
+console.log('Module_02 Task_07');
+
+console.log('-----');
+
+'use strict';
+
+function filterArray(array) {
+  
+    const numbers = [];
+    
+  for(let i = 0; i < array.length; i += 1) {
+      
+      if (Number.isFinite(array[i])) {
+          numbers.push(array[i]);
+      }
+  }
+  return numbers;
+}
+
+console.log(filterArray([-2, 0, 2]));
+// [-2, 0, 2]
+
+console.log(filterArray([1, NaN, Infinity]));
+// [1]
+
+console.log(filterArray([0, -0, 100, '100']));
+// [0, 0, 100]
+
+console.log(filterArray([undefined, false, null, [], 1]));
+// [1]
+
+console.log(filterArray([{}, () => {}, 2]));
+// [2]
+
+
+// Напиши функцию filterArray(array), принимающую 1 параметр array - массив.
+// Функция проверяет каждое значение массива - является ли значение конечным числом(Number.isFinite).
+// Для проверки используй цикл for.
+// Числа добавляются в новый массив numbers с помощью метода push, а не числа - игнорируются.
+// По завершению проверки массива array возвращается массив numbers.
